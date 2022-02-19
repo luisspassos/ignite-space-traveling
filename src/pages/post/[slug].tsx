c75bfc0d5a12b14395f8ccf6e20bbfb27c53a1a8
Luís Passos <luis.passos013@gmail.com>
2022-02-19 13:41:29 -0300

    data is being returned from getStaticProps

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,7 +1,10 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 
+import Prismic from '@prismicio/client';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { RichText } from 'prismic-dom';
 import { getPrismicClient } from '../../services/prismic';
-
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
 
@@ -26,16 +29,46 @@ interface PostProps {
   post: Post;
 }
 
-export default function Post(): JSX.Element {
-  return <h1>Post</h1>;
+export default function Post({ post }: PostProps): JSX.Element {
+  const readingTime = post.data.content.reduce((acc, el) => {
+    let a = '';
+    a += RichText.asText(el.body);
+
+    return a;
+  }, '');
+
+  console.log(readingTime);
+
+  return (
+    <article>
+      <h1>{post.data.title}</h1>
+      <time>
+        {format(new Date(post.first_publication_date), 'dd MMM yyyy', {
+          locale: ptBR,
+        })}
+      </time>
+      <span>{post.data.author}</span>
+    </article>
+  );
 }
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+export const getStaticPaths: GetStaticPaths = async () => {
+  const prismic = getPrismicClient();
+  const posts = await prismic.query([
+    Prismic.predicates.at('document.type', 'post'),
+  ]);
 
-//   // TODO
-// };
+  const uids = posts.results.map(post => ({
+    params: {
+      slug: post.uid,
+    },
+  }));
+
+  return {
+    paths: uids,
+    fallback: true,
+  };
+};
 
 export const getStaticProps: GetStaticProps = async context => {
   const { slug } = context.params;
@@ -43,10 +76,12 @@ export const getStaticProps: GetStaticProps = async context => {
   const prismic = getPrismicClient();
   const response = await prismic.getByUID('post', String(slug), {});
 
-  const post: Post = {
+  const post = {
+    uid: response.uid,
     first_publication_date: response.first_publication_date,
     data: {
       title: response.data.title,
+      subtitle: response.data.subtitle,
       banner: {
         url: response.data.banner.url,
       },
@@ -55,7 +90,7 @@ export const getStaticProps: GetStaticProps = async context => {
     },
   };
 
-  console.log(post);
+  console.log(JSON.stringify(post, null, 2));
 
   return {
     props: {
